2627768009ccfa35600ffd095eefdc7d93aa880b
kurugondalokesh <lokeshkurugonda2002@gmail.com>
2026-02-16 21:03:40 +0530

    terraform

---
+++ ec2/ec2.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "allow_ssh_terraform" {
+    name = "allow_sshh"
+    description = "allow all the ssh for terraform"
+
+    egress{
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    ingress{
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+}
